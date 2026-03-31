48f012b20a9761bdb621fb6f6b0f783c2c8fa37d
Rafael Almeida <rafaeldealmeida138@gmail.com>
2026-03-31 20:26:36 -0300

    01

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-Oi
+import sys
+from PyQt5 import uic, QtWidgets
+
+tela_menu = uic.loadUiType('telas/tela_menu.ui')[0]
+
+class menu(QtWidgets.QMainWindow, tela_menu):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+janela = menu()
+janela.show()
+
+sys.exit(app.exec())
