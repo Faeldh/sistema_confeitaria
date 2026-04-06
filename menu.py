47476b2d38e6b1041c2cd751f89b1b9dec7bc5b8
Rafael Almeida <rafaeldealmeida138@gmail.com>
2026-04-06 20:41:43 -0300

    01

---
+++ menu.py
@@ -0,0 +1,8 @@
+from PyQt5 import uic, QtWidgets
+
+tela_menu = uic.loadUiType('telas/tela_menu.ui')[0]
+
+class Menu(QtWidgets.QMainWindow, tela_menu):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
