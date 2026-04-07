134eadbb838ffbe9c328cb8414db00c63185abca
Rafael Almeida <rafaeldealmeida138@gmail.com>
2026-04-06 21:11:03 -0300

    01

--- conexao.py
+++ conexao.py
@@ -1,13 +1,20 @@
-import mysql.connector
+import pymysql
 
-print("TESTE DIRETO...")
+def conectar():
+    try:
+        print("🔄 Conectando com PyMySQL...")
 
-conexao = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    port=3306,
-    connection_timeout=5
-)
+        conexao = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='',
+            database='db_confeitaria',
+            port=3306
+        )
 
-print("CONECTOU!")
+        print("✅ Conectado com sucesso!")
+        return conexao
+
+    except Exception as erro:
+        print("❌ Erro ao conectar:", erro)
+        raise
