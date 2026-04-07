05cb8aacdd3e73076f6b8d63eb79e505a47dbeae
Rafael Almeida <rafaeldealmeida138@gmail.com>
2026-04-06 21:17:24 -0300

    01

--- conexao1.py
+++
@@ -1,8 +0,0 @@
-from conexao import conectar
-
-try:
-    conexao = conectar()
-    print("Conectou com sucesso!")
-    conexao.close()
-except Exception as erro:
-    print("Erro ao conectar:", erro)
